df12ca3503b7ed95dee4a4b7d7af3e59d9d2bc48
pbtronco <106410460+pbtronco@users.noreply.github.com>
2022-10-18 10:51:11 -0300

    Add files via upload

---
+++ atividade1/atividade1/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace atividade1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n1, n2, n3, n4, media;
+            Console.WriteLine("Digite o valor da primeira nota: ");
+            n1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da segunda nota: ");
+            n2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da terceira nota: ");
+            n3 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Digite o valor da quarta nota: ");
+            n4 = int.Parse(Console.ReadLine());
+            media = (n1 + n2 + n3 + n4) / 4;
+            Console.WriteLine(media);
+            if (n1 > media)
+            {
+                Console.WriteLine("Nota 1: "+n1);
+            }
+            if (n2 > media)
+            {
+                Console.WriteLine("Nota 2: "+n2);
+            }
+            if (n3 > media)
+            {
+                Console.WriteLine("Nota 3: "+n3);
+            }
+            if (n4 > media)
+            {
+                Console.WriteLine("Nota 4: "+n4);
+            }
+            else
+            {
+                Console.WriteLine("Nenhuma nota é maior que a média!");
+            }
+        }
+    }
+}
