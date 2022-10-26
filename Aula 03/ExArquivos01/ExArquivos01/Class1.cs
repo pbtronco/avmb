1e92f78d060769152826a79a37441f96977c265d
pbtronco <106410460+pbtronco@users.noreply.github.com>
2022-10-26 10:48:31 -0300

    Add files via upload
    Exercícios 01 - Arquivos

---
+++ Aula 03/ExArquivos01/ExArquivos01/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExArquivos01
+{
+    internal class Class1
+    {
+        string email, nome;
+        StreamWriter sw;
+        StreamReader sr;
+        public Class1(string nome)
+        {
+            this.nome = nome;
+        }
+        public void criarArquivo()
+        {
+            sw = new StreamWriter("C:\\Arquivo\\" + nome + ".txt", true, Encoding.UTF8);
+        }
+        public void lerArquivo()
+        {
+            string linha;
+            sr = new StreamReader("C:\\Arquivo\\" + nome + ".txt");
+            linha = sr.ReadLine();
+            while (linha != null)
+            {
+                Console.WriteLine(linha);
+                linha = sr.ReadLine();
+            }
+            sr.Close();
+        }
+        public void gravarMensagem(string email)
+        {
+            sw.WriteLine(email);
+            sw.Flush();
+        }
+        public void fecharArquivo()
+        {
+            sw.Close();
+        }
+    }
+}
---
+++ Aula 03/ExArquivos01/ExArquivos01/Program.cs
@@ -0,0 +1,43 @@
+﻿namespace ExArquivos01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string nomearquivo, mensagem;
+            int op;
+            Console.WriteLine("Digite o nome do arquivo para os emails");
+            nomearquivo = Console.ReadLine();
+            Class1 a = new Class1(nomearquivo);
+
+            while (true)
+            {
+                Console.WriteLine("Digite:\n1- Escrever um arquivo\n" +
+                    "2 - Gravar um Email\n" +
+                    "3 - Ler os Emails\n" +
+                    "0 - Para sair do programa");
+                op = int.Parse(Console.ReadLine());
+                switch (op)
+                {
+                    case 1:
+                        a.criarArquivo();
+                        a.fecharArquivo();
+                        break;
+                    case 2:
+                        a.criarArquivo();
+                        Console.WriteLine("Digite o Email:");
+                        string email = Console.ReadLine();
+                        a.gravarMensagem(email);
+                        a.fecharArquivo();
+                        break;
+                    case 3:
+                        a.lerArquivo();
+                        break;
+                    case 0:
+                        break;
+                }
+            
+            }
+        }
+    }
+}
