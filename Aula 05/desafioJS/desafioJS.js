22c1667bcd9608a49faa3186ceefd36519e5c8cd
pbtronco <106410460+pbtronco@users.noreply.github.com>
2022-11-07 09:03:54 -0300

    Add files via upload

---
+++ Aula 05/desafioJS/desafioJS.js
@@ -0,0 +1,13 @@
+var r = 0;
+function exalerta(){
+    var i;
+    r++;
+    alert(r);
+    if (r == 5){
+        alert("Limite");
+        r = 0;
+    }
+}
+function zeraContador(){
+    contador = 0;
+}
