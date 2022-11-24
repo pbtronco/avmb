6e355b154f053f131e078be4f95407677756c3a5
pbtronco <106410460+pbtronco@users.noreply.github.com>
2022-11-24 10:49:08 -0300

    Add files via upload

---
+++ Aula 08/Porta/Porta.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porta
+{
+    internal abstract class Porta
+    {
+        public string cor;
+        public double tamanho;
+        public string tipoFechadura;
+        public abstract void abre();
+    }
+}
---
+++ Aula 08/Porta/PortaNormal.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porta
+{
+    internal class PortaNormal : Porta
+    {
+        public override void abre()
+        {
+            cor = "preta";
+            tamanho = 1.55;
+            Console.WriteLine("Porta abre normal " + cor);
+        }
+    }
+}
