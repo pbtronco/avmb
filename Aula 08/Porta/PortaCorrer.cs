7a761435ac9373bc8bbb1788fb9f6da189aaaf51
pbtronco <106410460+pbtronco@users.noreply.github.com>
2022-12-02 09:06:41 -0300

    Add files via upload

---
+++ Aula 08/Porta/PortaCorrer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Porta
+{
+    internal class PortaCorrer : Porta
+    {
+        public override void abre()
+        {
+            cor = "transparente";
+            tamanho = 2.33;
+            Console.WriteLine("Porta correr arrastando "+ cor);
+        }
+    }
+}
